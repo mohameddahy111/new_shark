7ee2d31f8ff70af1e2e84b0ae639637b8b223a76
mohameddahy dahy <mohameddahy111@gmail.com>
2023-12-20 23:06:06 +0200

    updata

--- src/app/context/DataStore.js
+++ src/app/context/DataStore.js
@@ -1,12 +1,13 @@
 "use client"
 
 import { useMediaQuery } from "@mui/material";
-import {createContext, useContext} from "react";
+import {createContext, useContext, useState} from "react";
 
 const DataStore = createContext();
 export const DataStoreProvider = ({ children }) => {
-    const mobileDriver = useMediaQuery("(max-width:600px)")
-  return <DataStore.Provider value={{mobileDriver }}> {children} </DataStore.Provider>;
+  const mobileDriver = useMediaQuery("(max-width:600px)")
+  const [user , setUsers]=useState([])
+  return <DataStore.Provider value={{mobileDriver  , user , setUsers}}> {children} </DataStore.Provider>;
 };
 
 export const Store = () => {
